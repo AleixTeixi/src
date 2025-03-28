ee0d770066d0fcb5dbae7e2f85fefdc29417d7f5
AleixTeixi <aleixteixidor2004@gmail.com>
2025-03-28 19:01:42 +0100

    canvis fets en Regio.java

--- Regio.java
+++ Regio.java
@@ -1,5 +1,21 @@
+//Falta implementar constructors...
+
+import java.util.ArrayList;
 public class Regio {
     // Atributs
+
+    private String nomRegio; 
+    private int nombreHabitants; 
+
+    class LlistatRegions{
+        public String nomRegio;
+        public boolean confinada; // true si està confinada
+    }
+
+    ArrayList<LlistatRegions> regionsVeines;
+
+    ArrayList<String> _regionsVeines;
+
 
     // Mètodes
 
@@ -17,5 +33,22 @@ public class Regio {
     public void desconfinarDur() {
 
     }
-}
+
+    public boolean esVeina(String nomRegio){
+    // Pre: regio existeix amb nomRegio
+    // Post: retorna true si el nom de la regio passada és veina, false altrament
+    }
+
+    public boolean esConfinada(String nomRegio){
+    // Pre: regio existeix amb nomRegio
+    // Post: retorna true si el nom de la regió passada com a paràmetre és veina i confinada, fals altrament
+        for(...){
+            if(regionsVeines[i]._regio.equals(nomRegio)){
+                return regionsVeines[i]._confinada;
+            }
+        }
+    }
+
+    
+}   
 
